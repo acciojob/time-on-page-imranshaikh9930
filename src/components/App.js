88998ea31ab04b8b5f7d2882c10acf9d01cf1b5b
Imran shaikh <imranpathan7040@gmail.com>
2024-08-22 15:30:38 +0530

    timer added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [sec,setSec] = useState(0);
+
+  useEffect(()=>{
+   let timerInterval =  setInterval(()=>{
+      setSec(prev=>prev+1);
+      console.log();
+    },1000)
+
+    return ()=>clearInterval(timerInterval);
+  },[])
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Imran</h1>
+        <p>You've been on this page for {sec} seconds</p>
     </div>
   )
 }
